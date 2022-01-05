df64efa7b251d7b9aa19bf2fe18edbbbf71db556
Bhagyeshpro <bhagyeshpaunikar2001@gmail.com>
2022-01-05 03:49:54 -0800

    Login Done!

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './src/screens/HomeScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./src/screens/LoginScreen"
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-      <HomeScreen/>
+      <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          options={{ headerShown: false,
+           }}
+          name="Login"
+          component={LoginScreen}
+        />
+         <Stack.Screen
+          options={{ headerShown: false,
+           }}
+          name="Home"
+          component={HomeScreen}
+        />
+
+          
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
